66747e7423a8518f9c7a43b39c11ff3cb6b071d1
Wiko <wico216@gmail.com>
2016-04-26 13:25:38 -0400

    Added Global Discount class

---
+++ Orlandos example/Global_Discount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orlandos_example
+{
+    class Global_Discount
+    {
+    }
+}
